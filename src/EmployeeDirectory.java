4d9bddcef92327292a2e02c0431c101cbb3a91b0
Rutuja Pujare <39995604+rutu11@users.noreply.github.com>
2025-03-09 22:51:21 +0530

    Update EmployeeDirectory.java

--- src/EmployeeDirectory.java
+++ src/EmployeeDirectory.java
@@ -1,8 +1,6 @@
 import java.time.LocalDate;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class EmployeeDirectory {
     public static void main(String[] args) {
 
